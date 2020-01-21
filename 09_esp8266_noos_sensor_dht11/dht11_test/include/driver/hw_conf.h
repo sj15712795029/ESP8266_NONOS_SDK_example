f8a720af85cc83a6b04d9b1ae048947b288b4263
zhongjun.yu <124756828@qq.com>
2020-01-21 15:45:24 +0800

    增加dht11温湿度传感器测试工程代码

---
+++ 09_esp8266_noos_sensor_dht11/dht11_test/include/driver/hw_conf.h
@@ -0,0 +1,14 @@
+/******************************************************************************
+  * @file    hw_conf.h
+  * @author  Yu-ZhongJun
+  * @version V0.0.1
+  * @date    2020-01-20
+  * @brief   hw config header file
+******************************************************************************/
+#ifndef HW_CONF_H_H_H
+#define HW_CONF_H_H_H
+
+#define DEBUG_ENABLE	1
+
+
+#endif
